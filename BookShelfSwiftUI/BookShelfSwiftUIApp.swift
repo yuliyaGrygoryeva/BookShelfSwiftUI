81dc3514752530eb3c7eea7f0dc8864992297a9a
Yuliya Grygoryeva <jgrygoryeva@gmail.com>
2022-07-07 13:38:51 -0700

    Initial commit

---
+++ BookShelfSwiftUI/BookShelfSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookShelfSwiftUIApp.swift
+//  BookShelfSwiftUI
+//
+//  Created by Yuliya  on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct BookShelfSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
